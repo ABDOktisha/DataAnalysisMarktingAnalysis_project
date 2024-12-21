683184748f8c932828728c8fc0700f958efac371
ABDOktisha <132022006+ABDOktisha@users.noreply.github.com>
2024-12-21 16:00:58 +0200

    Add files via upload

---
+++ dim_customer.sql
@@ -0,0 +1,5 @@
+
+select c.CustomerID, c.CustomerName, c.Email, c.Gender,c.Age,
+	   g.City, g.Country -- select country and sity from grography table
+from [dbo].[customers] c left join [dbo].[geography] g
+on c.GeographyID=g.GeographyID
