9b82d42edc4b7f1b743a51072f530c642782d020
ktisha <abdoktisha55@gmail.com>
2024-12-22 17:41:12 +0200

    remove SQL

--- dim_customer.sql
+++
@@ -1,5 +0,0 @@
-
-select c.CustomerID, c.CustomerName, c.Email, c.Gender,c.Age,
-	   g.City, g.Country -- select country and sity from grography table
-from [dbo].[customers] c left join [dbo].[geography] g
-on c.GeographyID=g.GeographyID
